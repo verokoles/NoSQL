8a1ef3dad73086b9c7aa29254af989297ddf9270
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-06-12 02:20:53 -0500

    connecting to atlas, linking project to mongo to run

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(require("./routes"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
+mongoose.connect(process.env.MONGODB_URI || 
+  'mongodb+srv://verkoles:<Veronichka88$$>@clusterv.sl7m0.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -18,6 +20,4 @@ mongoose.set('debug', true);
 
 app.use(require('./routes'));
 
-app.listen(PORT, () => console.log(
-  `🌍 Connected on localhost:${PORT}`
-  ));
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
